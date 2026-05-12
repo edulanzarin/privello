4c46b79c28a58760db452b7f4702ad02db8092e8
edulanzarin <edulanzarin@outlook.com>
2026-05-12 20:18:41 -0300

    fix: allowedDevOrigins para rede local, inclui .env

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.1.93"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
